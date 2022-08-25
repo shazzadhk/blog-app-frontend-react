d89a7f21c7c56664dac30eb7a561bcd828914dd5
shazzadhk <shazzadhk12@gmail.com>
2022-08-25 12:39:06 +0600

    change in About component

--- src/About.js
+++ src/About.js
@@ -2,7 +2,8 @@ const About = () => {
     return(
         <>
             <h1>This is About Page</h1>
-		 <p>You can find all details of this project in this page</p>
         </>
     )
 }
+
+export default About;
