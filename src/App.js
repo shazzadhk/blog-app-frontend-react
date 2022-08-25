1e3d2688b2b643f39e5d4c7be8db3e61bcb1912e
shazzadhk <shazzadhk12@gmail.com>
2022-08-25 11:07:42 +0600

    change in About and App Component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from './Home';
+import About from './About';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/home' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
       </Routes>
     </BrowserRouter>
   );
